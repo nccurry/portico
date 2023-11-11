157be6dc17ad267205fab12aa53621276395d7df
Nicholas Curry <ncurry@nvidia.com>
2023-11-11 17:47:26 -0600

    Nitpick everything

---
+++ src/streamlit_app/Home.py
@@ -0,0 +1,125 @@
+from datetime import datetime
+import pandas as pd
+from src.utils.utils import load_data, initialize_session_state
+import streamlit as st
+
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+
+# Initialize
+load_data()
+initialize_session_state()
+
+# Data
+checking_df = st.session_state.balance_history_scrubbed_data
+checking_df = checking_df.sort_values(by='Date')
+checking_df = checking_df.drop_duplicates('Account ID', keep='last')
+checking_df = checking_df[checking_df["Group"] == "Checking"]
+checking_df = checking_df.filter(["Account", "Balance"])
+checking_total = float(checking_df["Balance"].sum())
+
+saving_df = st.session_state.balance_history_scrubbed_data
+saving_df = saving_df.sort_values(by='Date')
+saving_df = saving_df.drop_duplicates('Account ID', keep='last')
+saving_df = saving_df[saving_df["Group"] == "Saving"]
+saving_df = saving_df.filter(["Account", "Balance"])
+saving_total = float(saving_df["Balance"].sum())
+
+credit_df = st.session_state.balance_history_scrubbed_data
+credit_df = credit_df.sort_values(by='Date')
+credit_df = credit_df.drop_duplicates('Account ID', keep='last')
+credit_df = credit_df[credit_df["Group"] == "Credit Card"]
+credit_df = credit_df.filter(["Account", "Balance"])
+credit_total = credit_df["Balance"].sum()
+
+investment_df = st.session_state.balance_history_scrubbed_data
+investment_df = investment_df.sort_values(by='Date')
+investment_df = investment_df.drop_duplicates('Account ID', keep='last')
+investment_df = investment_df[investment_df["Group"] == "Investment"]
+investment_df = investment_df.filter(["Account", "Balance"])
+investment_total = investment_df["Balance"].sum()
+
+# UI
+
+col1, col2, col3 = st.columns(3)
+
+col1.header("Checking")
+col1.subheader(f'${"{:,.2f}".format(checking_total)}')
+col1.dataframe(
+    data=checking_df,
+    hide_index=True,
+    width=300,
+    column_config={
+        "Account": st.column_config.Column(
+            width="small"
+        ),
+        "Balance": st.column_config.NumberColumn(
+            format="$ %.2f"
+        )
+    }
+)
+
+col1.header("Saving")
+col1.subheader(f'${"{:,.2f}".format(saving_total)}')
+col1.dataframe(
+    data=saving_df,
+    hide_index=True,
+    width=300,
+    column_config={
+        "Account": st.column_config.Column(
+            width="small"
+        ),
+        "Balance": st.column_config.NumberColumn(
+            format="$ %.2f"
+        )
+    }
+)
+
+
+col2.header("Credit")
+col2.subheader(f'-${"{:,.2f}".format(credit_total)}')
+col2.dataframe(
+    data=credit_df,
+    hide_index=True,
+    width=300,
+    column_config={
+        "Account": st.column_config.Column(
+            width="small"
+        ),
+        "Balance": st.column_config.NumberColumn(
+            format="$ %.2f"
+        )
+    }
+)
+
+
+col3.header("Investments")
+col3.subheader(f'${"{:,.2f}".format(investment_total)}')
+col3.dataframe(
+    data=investment_df,
+    hide_index=True,
+    width=300,
+    column_config={
+        "Account": st.column_config.Column(
+            width="small"
+        ),
+        "Balance": st.column_config.NumberColumn(
+            format="$ %.2f"
+        )
+    }
+)
+
+st.title("Recent Transactions")
+
+transactions_df = st.session_state.transactions_scrubbed_data
+transactions_df = transactions_df[transactions_df["Date"] > datetime.now() - pd.to_timedelta("10day")]
+transactions_df = transactions_df.sort_values(by='Date', ascending=False)
+transactions_df = transactions_df.filter(["Date", "Full Description", "Amount", "Account", "Category", "Group"])
+st.dataframe(
+    data=transactions_df,
+    hide_index=True,
+    column_config={
+        "Date": st.column_config.DateColumn(
+            disabled=True,
+        )
+    }
+)
---
+++ src/utils/spreadsheet.py
@@ -0,0 +1,87 @@
+import streamlit as st
+from streamlit.runtime.state import SessionStateProxy
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+from abc import ABCMeta, abstractmethod
+
+
+class Spreadsheet(metaclass=ABCMeta):
+    """Class used to interact with Spreadsheet data in Google Sheets"""
+    name: str
+    url: str
+    raw_df: pd.DataFrame
+    scrubbed_df: pd.DataFrame
+
+    def __init__(self, name: str, url: str) -> None:
+        self.name = name
+        self.url = url
+        self.load()
+        self.scrub()
+
+    def load(self) -> None:
+        """Load data from the external spreadsheet"""
+        conn = st.connection(name=self.name, type=GSheetsConnection)
+        self.raw_df = conn.read(spreadsheet=self.url)
+
+    @abstractmethod
+    def scrub(self) -> None:
+        """Clean up the data stored in self.raw_df"""
+        ...
+
+    def cache(
+            self, session_state: SessionStateProxy,
+            state_prefix: str = "ss",
+            force: bool = False
+    ) -> None:
+        """Cache the Spreadsheet data in the Streamlit session state"""
+        if f"{self.name}_raw_df" not in session_state or force:
+            session_state[f"{state_prefix}_{self.name}_raw_df"] = self.raw_df
+
+        if f"{self.name}_scrubbed_df" not in session_state or force:
+            session_state[f"{state_prefix}_{self.name}_scrubbed_df"] = self.scrubbed_df
+
+
+class TransactionSpreadsheet(Spreadsheet):
+    def scrub(self) -> None:
+        df = self.raw_df.copy()
+
+        # Drop empty column
+        df = df.drop("Unnamed: 0", axis=1)
+
+        # Recast Amount column as float
+        df["Amount"] = df["Amount"].replace('[\$,]', '', regex=True).astype(float)
+
+        # Recast dates as datetime
+        df["Date"] = pd.to_datetime(df["Date"])
+        df["Month"] = pd.to_datetime(df["Month"])
+        df["Week"] = pd.to_datetime(df["Week"])
+        df["Date Added"] = pd.to_datetime(df["Date Added"])
+        df["Categorized Date"] = pd.to_datetime(df["Categorized Date"])
+
+        # Use better strings for Month and Week columns
+        df["Month"] = df["Month"].dt.strftime('%Y-%m')
+        df["Week"] = df["Week"].dt.strftime('%U')
+
+        self.scrubbed_df = df
+
+
+class BalanceHistorySpreadsheet(Spreadsheet):
+    def scrub(self) -> None:
+        df = self.raw_df.copy()
+
+        # Drop empty column
+        df = df.drop("Unnamed: 0", axis=1)
+
+        # Recast Amount column as float
+        df["Balance"] = df["Balance"].replace('[\$,]', '', regex=True).astype(float)
+
+        # Recast dates as datetime
+        df["Date"] = pd.to_datetime(df["Date"])
+        df["Time"] = pd.to_datetime(df["Time"])
+        df["Month"] = pd.to_datetime(df["Month"])
+        df["Week"] = pd.to_datetime(df["Week"])
+        df["Date Added"] = pd.to_datetime(df["Date Added"])
+
+        # Use better strings for Month and Week columns
+        df["Month"] = df["Month"].dt.strftime('%Y-%m')
+        df["Week"] = df["Week"].dt.strftime('%U')
