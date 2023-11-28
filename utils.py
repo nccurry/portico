1968db863315a9ad700be3d26c1ac9bf9fa691a9
Nicholas Curry <ncurry@nvidia.com>
2023-11-28 07:57:02 -0600

    Add last month filter

--- utils.py
+++ utils.py
@@ -8,9 +8,10 @@ def first_day_of_the_month(relative_months: int = 0) -> datetime.datetime:
     return first_day_of_this_month + dateutil.relativedelta.relativedelta(months=relative_months)
 
 
-def last_day_of_the_month() -> datetime:
+def last_day_of_the_month(relative_months: int = 0) -> datetime:
     """Return the last day of the month as a datetime"""
+    first_day_of_the_relative_month = first_day_of_the_month(relative_months=relative_months)
     # The day 28 exists in every month. 4 days later, it's always next month
-    next_month = datetime.datetime.today().replace(day=28) + datetime.timedelta(days=4)
+    next_month = first_day_of_the_relative_month.replace(day=28) + datetime.timedelta(days=4)
 
     return next_month - datetime.timedelta(days=next_month.day)
